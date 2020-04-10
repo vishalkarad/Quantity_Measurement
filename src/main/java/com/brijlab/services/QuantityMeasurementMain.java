aaeac082f4451b5c8fc3de81519163c2b0b1fd66
vishalkarad <vishalkarad777@gmail.com>
2020-04-10 12:45:23 +0530

    Added : Add if condition to throw Null Pointer Exception

--- src/main/java/com/brijlab/services/QuantityMeasurementMain.java
+++ src/main/java/com/brijlab/services/QuantityMeasurementMain.java
@@ -2,7 +2,9 @@ package com.brijlab.services;
 
 public class QuantityMeasurementMain {
 
-    public boolean compare(UnitComparetor unitComparetor1,UnitComparetor unitComparetor2){
+    public boolean compare(UnitComparetor unitComparetor1,UnitComparetor unitComparetor2) throws QuantityMeasurementException {
+        if (unitComparetor1 == null || unitComparetor2 == null)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_POINTER_EXCEPTION, "Null Object");
         return unitComparetor1.equals(unitComparetor2);
     }
 }
