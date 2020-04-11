a3fc865b17d10bcd402e0c36d1ae4b9fa95df4d5
vishalkarad <vishalkarad777@gmail.com>
2020-04-11 18:30:27 +0530

    Added : add kilogram grams And tonne in enum

--- src/main/java/com/brijlab/services/Length.java
+++ src/main/java/com/brijlab/services/Length.java
@@ -3,7 +3,7 @@ package com.brijlab.services;
 public enum Length {
     FEET(  12), INCH(1 ),YARD (36),
     CENTIMETER(0.4),GALLON(3.78),LITRE(1),
-    ML(0.001);
+    ML(0.001),KILOGRAMS(1),GRAM(0.001),TONNE(1000),;
 
     public final double value;
 
