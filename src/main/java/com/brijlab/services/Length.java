fcaf47e120746faffebaac350248140bd875124f
vishalkarad <vishalkarad777@gmail.com>
2020-04-11 17:19:06 +0530

    Added : add valu of gallon and litre in enum

--- src/main/java/com/brijlab/services/Length.java
+++ src/main/java/com/brijlab/services/Length.java
@@ -2,7 +2,7 @@ package com.brijlab.services;
 
 public enum Length {
     FEET(  12), INCH(1 ),YARD (36),
-    CENTIMETER(0.4);
+    CENTIMETER(0.4),GALLON(3.78),LITRE(1);
 
     public final double value;
 
