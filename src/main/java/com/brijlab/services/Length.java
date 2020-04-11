00e041e312d78bfeca9468ea0de6c5eade390458
vishalkarad <vishalkarad777@gmail.com>
2020-04-11 17:30:42 +0530

    Added : add ML value in enum

--- src/main/java/com/brijlab/services/Length.java
+++ src/main/java/com/brijlab/services/Length.java
@@ -2,7 +2,8 @@ package com.brijlab.services;
 
 public enum Length {
     FEET(  12), INCH(1 ),YARD (36),
-    CENTIMETER(0.4),GALLON(3.78),LITRE(1);
+    CENTIMETER(0.4),GALLON(3.78),LITRE(1),
+    ML(0.001);
 
     public final double value;
 
