8ce99196f38e2f5ef66a38e30ef5f151c0e46a26
vishalkarad <vishalkarad777@gmail.com>
2020-04-11 21:50:35 +0530

    Added : add commits

--- src/main/java/com/brijlab/services/QuantityMeasurementMain.java
+++ src/main/java/com/brijlab/services/QuantityMeasurementMain.java
@@ -2,12 +2,13 @@ package com.brijlab.services;
 
 public class QuantityMeasurementMain {
 
+    // add compare method to check override equals method
     public boolean compare(UnitComparetor unitComparetor1,UnitComparetor unitComparetor2) throws QuantityMeasurementException {
         if (unitComparetor1 == null || unitComparetor2 == null)
             throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_POINTER_EXCEPTION, "Null Object");
         return unitComparetor1.equals(unitComparetor2);
     }
-
+    // Addition of two objects
     public double addition(UnitComparetor unitComparetor, UnitComparetor unitComparetor2) {
         return unitComparetor.getValue()+unitComparetor2.getValue();
     }
