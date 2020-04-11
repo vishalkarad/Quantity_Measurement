c447fc621ed47f3612798005baebc1bff46cb2b1
vishalkarad <vishalkarad777@gmail.com>
2020-04-11 16:09:37 +0530

    Added : Add centimeter value in enum

--- src/main/java/com/brijlab/services/Length.java
+++ src/main/java/com/brijlab/services/Length.java
@@ -1,7 +1,8 @@
 package com.brijlab.services;
 
 public enum Length {
-    FEET(  12), INCH(1 ),YARD (36);
+    FEET(  12), INCH(1 ),YARD (36),
+    CENTIMETER(0.4);
 
     public final double value;
 
